a971729c05a81b97942ca783ad3002e8fceb982e
Igor Markin <9588604@gmail.com>
2022-10-01 13:05:50 +0300

    added list_display

--- monitoring_system/app/monitoring/admin.py
+++ monitoring_system/app/monitoring/admin.py
@@ -4,10 +4,31 @@ from .models import WebServer, WebServerRequest
 
 @admin.register(WebServer)
 class WebServerAdmin(admin.ModelAdmin):
-    list_filter = ('status',)
-    search_fields = ('name', 'url')
+    list_display = (
+        'name',
+        'url',
+        'status',
+    )
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'name',
+        'url',
+    )
 
 
 @admin.register(WebServerRequest)
 class WebServerRequestAdmin(admin.ModelAdmin):
-    list_filter = ('webserver', 'status_code', 'status')
+    list_display = (
+        'webserver',
+        'started_at',
+        'latency',
+        'status_code',
+        'status',
+    )
+    list_filter = (
+        'webserver',
+        'status_code',
+        'status',
+    )
