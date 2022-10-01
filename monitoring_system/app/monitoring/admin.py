b2fbb5ee9f2c2afbf0c7249485d7eb94dea258e9
Igor Markin <9588604@gmail.com>
2022-10-01 12:20:56 +0300

    added WebServer, WebServerRequest admin classes

--- monitoring_system/app/monitoring/admin.py
+++ monitoring_system/app/monitoring/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import WebServer, WebServerRequest
 
-# Register your models here.
+
+@admin.register(WebServer)
+class WebServerAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+    search_fields = ('name', 'url')
+
+
+@admin.register(WebServerRequest)
+class WebServerRequestAdmin(admin.ModelAdmin):
+    list_filter = ('webserver', 'status_code', 'status')
